7cda0973d05be4492c119a4a338c48ccaa573eb8
artiya4u <artiya4u@gmail.com>
2018-03-03 18:16:58 +0700

    first commit

---
+++ contracts/BNK48Coin.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.18;
+
+import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+
+
+contract BNK48Coin is MintableToken {
+
+    string public constant name = "BNK48 COIN"; // solium-disable-line uppercase
+    string public constant symbol = "BNK48"; // solium-disable-line uppercase
+    uint8 public constant decimals = 18; // solium-disable-line uppercase
+
+}
